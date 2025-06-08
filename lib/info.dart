44002aaa541d9408b9caf39069a72474c77a65fd
kdbordios <kcdbordios.work@gmail.com>
2025-06-09 07:05:29 +0800

    Created new Dart files to separate specific logic

---
+++ lib/info.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void memberInfo(BuildContext context, int memberId) {
+  String name = '';
+  String fullName = '';
+  String birthdate = '';
+  String position = '';
+  String description = '';
+
+  switch (memberId) {
+    case 1:
+      name = 'Taeyeon';
+      fullName = 'Kim Tae-yeon';
+      birthdate = 'March 9, 1989';
+      position = 'Leader; Main Vocalist';
+      description = 'Leader and main vocalist of Girls\' Generation. Also a successful soloist.';
+      break;
+    case 2:
+      name = 'Jessica';
+      fullName = 'Jessica Sooyeon Jung / Jung Soo-yeon';
+      birthdate = 'April 18, 1989';
+      position = 'Main Vocalist';
+      description = 'Former member who left the group in 2014. Now runs a fashion business.';
+      break;
+    case 3:
+      name = 'Sunny';
+      fullName = 'Susan Soonkyu Lee / Lee Soon-kyu';
+      birthdate = 'May 15, 1989';
+      position = 'Lead Vocalist; Sub-Rapper';
+      description = 'Niece of SM Entertainment\'s founder. Known for \'aegyo\' expressions.';
+      break;
+    case 4:
+      name = 'Tiffany Young';
+      fullName = 'Stephanie Young Hwang / Hwang Mi-young';
+      birthdate = 'August 1, 1989';
+      position = 'Lead Vocalist; Sub-Rapper';
+      description = 'Had a brief solo career outside of K-Pop';
+      break;
+    case 5:
+      name = 'Hyo';
+      fullName = 'Kim Hyo-yeon';
+      birthdate = 'September 22, 1989';
+      position = 'Main Dancer; Main Rapper; Sub-Vocalist';
+      description = 'Nicknamed \'Dancing Queen\' and now a DJ under the name HYO.';
+      break;
+    case 6:
+      name = 'Kwon Yuri';
+      fullName = 'Kwon Yu-ri';
+      birthdate = 'December 5, 1989';
+      position = 'Main Dancer / Lead Rapper / Sub-Vocalist';
+      description = 'Also known as an actress and for her cooking.';
+      break;
+    case 7:
+      name = 'SooYoung';
+      fullName = 'Choi Soo-young';
+      birthdate = 'February 10, 1990';
+      position = 'Lead Dancer; Lead Rapper; Sub-Vocalist';
+      description = 'Started her career in Japan and now an actress who recently made her Hollywood debut with \'Ballerina\',';
+      break;
+    case 8:
+      name = 'Yoona';
+      fullName = 'Im Yoon-ah / Lim Yoona';
+      birthdate = 'May 30, 1990';
+      position = 'Lead Dancer; Lead Rapper; Sub-Vocalist; Center; Visual';
+      description = 'Face of the group and successful actress who is also known as one of the most beautiful faces in South Korea';
+      break;
+    case 9:
+      name = 'Seohyun';
+      fullName = 'Seo Ju-hyun';
+      birthdate = 'June 28, 1991';
+      position = 'Lead Vocalist; Maknae';
+      description = 'Actress who also participated in musical plays';
+      break;
+    default:
+      name = 'Unknown';
+      description = 'No information available.';
+  }
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        backgroundColor: Colors.pink[100], // Light/carnation pink
+        title: Text(name, style: const TextStyle(color: Colors.black)),
+        content: Text('$fullName\n$birthdate\n$position\n$description', style: const TextStyle(color: Colors.black)),
+        actions: <Widget>[
+          TextButton(child: const Text('Close', style: TextStyle(color: Colors.black)), onPressed: () => Navigator.of(context).pop()),
+        ],
+      );
+    },
+  );
+}
---
+++ lib/menu.dart
@@ -0,0 +1,173 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'info.dart';
+
+class GGView extends StatefulWidget {
+  const GGView({super.key});
+
+  @override
+  GGViewState createState() => GGViewState();
+}
+class GGViewState extends State<GGView> {
+  final PageController _pageController = PageController();
+
+  int _currentPage = 0;
+
+  void _nextPage() {
+    if (_currentPage < 2) {
+      setState(() => _currentPage++);
+      _pageController.animateToPage(_currentPage, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+    }
+  }
+
+  void _previousPage() {
+    if (_currentPage > 0) {
+      setState(() => _currentPage--);
+      _pageController.animateToPage(_currentPage, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: _pageController,
+            onPageChanged: (index) => setState(() => _currentPage = index),
+            children: [
+              Container(color: Colors.pinkAccent, child: const Center(child: Text("Welcome Page", textAlign: TextAlign.center, style: TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold)))),
+              Scaffold(
+                appBar: AppBar(title: const Text("Girls' Generation Members")),
+                backgroundColor: Colors.pinkAccent,
+                body: SafeArea(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          memberContainer('images/taeyeon.png', 'Taeyeon', () => memberInfo(context, 1)),
+                          memberContainer('images/jessica.png', 'Jessica (formerly)', () => memberInfo(context, 2)),
+                          memberContainer('images/sunny.png', 'Sunny', () => memberInfo(context, 3)),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          memberContainer('images/tiffany.png', 'Tiffany Young', () => memberInfo(context, 4)),
+                          memberContainer('images/hyoyeon.png', 'Hyo', () => memberInfo(context, 5)),
+                          memberContainer('images/yuri.png', 'Kwon Yuri', () => memberInfo(context, 6)),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          memberContainer('images/sooyoung.png', 'SooYoung', () => memberInfo(context, 7)),
+                          memberContainer('images/yoona.png', 'Yoona', () => memberInfo(context, 8)),
+                          memberContainer('images/seohyun.png', 'Seohyun', () => memberInfo(context, 9)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Page 3: SNSDice
+              Scaffold(
+                appBar: AppBar(
+                  title: const Text("Sonyeo Si-dice (Dice Generation)"),
+                  backgroundColor: Colors.pink,
+                ),
+                backgroundColor: Colors.pinkAccent,
+                body: const SNSDicePage(),
+              ),
+            ],
+          ),
+          Positioned(
+            left: 10,
+            top: MediaQuery.of(context).size.height / 2 - 30,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: _previousPage,
+            ),
+          ),
+          Positioned(
+            right: 10,
+            top: MediaQuery.of(context).size.height / 2 - 30,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_forward, color: Colors.white),
+              onPressed: _nextPage,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Function to create a member container
+  Widget memberContainer(String imagePath, String name, VoidCallback onTap) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            decoration: BoxDecoration(image: DecorationImage(image: AssetImage(imagePath), fit: BoxFit.cover)),
+          ),
+        ),
+        const SizedBox(height: 8.0), // Space between the image and text
+        Text(name, style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 10.0),),
+      ],
+    );
+  }
+}
+
+class SNSDicePage extends StatefulWidget {
+  const SNSDicePage({super.key});
+
+  @override
+  SNSDicePageState createState() => SNSDicePageState();
+}
+class SNSDicePageState extends State<SNSDicePage> {
+  int leftMemberIndex = 1;
+  int rightMemberIndex = 1;
+
+  void changeMemberFaces() {
+    setState(() {
+      leftMemberIndex = Random().nextInt(9) + 1; // 9 members
+      rightMemberIndex = Random().nextInt(9) + 1; // 9 members
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset(
+                'images/igab$leftMemberIndex.png',
+              ),
+              onPressed: () {
+                changeMemberFaces();
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset(
+                'images/igab$rightMemberIndex.png',
+              ),
+              onPressed: () {
+                changeMemberFaces();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
