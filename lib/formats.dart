958887a2cb48d540189125cdf67aa691d18ec587
LEVONO <kcdbordios.work@gmail.com>
2025-07-20 11:41:35 +0800

    Added custom SizedBox height

--- lib/formats.dart
+++ lib/formats.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const TextStyle textStyle = TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold);
+
+SizedBox sbh(BuildContext context, double pct) => SizedBox(height: MediaQuery.of(context).size.height * (pct / 100));
