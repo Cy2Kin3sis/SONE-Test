3b30028021b3813f7385b1e8623868be1a2a858d
LEVONO <kcdbordios.work@gmail.com>
2025-07-27 13:03:16 +0800

    New menu design

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'contents.dart';
+import 'formats.dart';
 
 class GGView extends StatefulWidget {
   const GGView({super.key});
@@ -10,16 +11,16 @@ class GGView extends StatefulWidget {
 class GGViewState extends State<GGView> {
   @override
   Widget build(BuildContext context) => DefaultTabController(
-    length: 3, // Three tabs/pages
+    length: 2, // Three tabs/pages
     child: Scaffold(
       appBar: AppBar(
         title: const Text("Girls' Generation"),
         backgroundColor: Colors.pink,
         bottom: const TabBar(
-          indicatorColor: Colors.white,
+          indicatorColor: white,
           tabs: [
-            Tab(icon: Icon(Icons.groups, color: Colors.white), child: Text("Members", style: TextStyle(color: Colors.white))),
-            Tab(icon: Icon(Icons.casino, color: Colors.white), child: Text("SNSDice", style: TextStyle(color: Colors.white))),
+            Tab(icon: Icon(Icons.groups, color: white), child: Text("Members", style: TextStyle(color: white))),
+            Tab(icon: Icon(Icons.casino, color: white), child: Text("SNSDice", style: TextStyle(color: white))),
           ],
         ),
       ),
