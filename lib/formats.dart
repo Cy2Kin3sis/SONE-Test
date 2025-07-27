bdca5ef8c394eb150eec28d68e40fa269b742273
LEVONO <kcdbordios.work@gmail.com>
2025-07-27 13:01:31 +0800

    Added reusable color for white for less space

--- lib/formats.dart
+++ lib/formats.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 const TextStyle textStyle = TextStyle(fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold);
 
+const Color white = Colors.white;
+
 SizedBox sbh(BuildContext context, double pct) => SizedBox(height: MediaQuery.of(context).size.height * (pct / 100));
