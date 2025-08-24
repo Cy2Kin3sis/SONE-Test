7ea53f7ed37325a87515c0ae5988aa2f5a3a0f63
LEVONO <kcdbordios.work@gmail.com>
2025-08-24 10:20:57 +0800

    Main documentation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+/// main.dart
+///
+/// This file contains the core of the app.
+library;
+
 import 'package:flutter/material.dart';
 import 'formats.dart';
 import 'menu.dart';
@@ -5,20 +10,19 @@ import 'menu.dart';
 void main() => runApp(const GirlsGen());
 
 class GirlsGen extends StatelessWidget {
+  /// App.
   const GirlsGen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        fontFamily: 'Segoe Print',
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.pink,
-          titleTextStyle: TextStyle(color: white, fontSize: 20, fontFamily: 'Segoe Print'),
-        ),
+  Widget build(BuildContext context) => MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'Segoe Print',
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.pink,
+        titleTextStyle: TextStyle(color: white, fontSize: 20, fontFamily: 'Segoe Print'),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const GGView(),
-    );
-  }
+    ),
+    debugShowCheckedModeBanner: false,
+    home: const GGView(),
+  );
 }
