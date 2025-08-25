290121c98dbb65fc0f362547ae8f69c11aa2a14c
LEVONO <kcdbordios.work@gmail.com>
2025-08-25 19:05:04 +0800

    Added SizedBox reusable variables

--- lib/formats.dart
+++ lib/formats.dart
@@ -9,6 +9,10 @@ const TextStyle textStyle = TextStyle(fontSize: 30, color: Colors.white, fontWei
 
 const Color white = Colors.white;
 
-//SizedBox sbhf
+SizedBox sbhf(double height) => SizedBox(height: height);
+
+SizedBox sbwf(double width) => SizedBox(width: width);
 
 SizedBox sbhc(BuildContext context, double pct) => SizedBox(height: MediaQuery.of(context).size.height * (pct / 100));
+
+SizedBox sbwc(BuildContext context, double pct) => SizedBox(height: MediaQuery.of(context).size.height * (pct / 100));
